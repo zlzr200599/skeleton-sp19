c6fdff9caa51ce59eb2e852e6b8ffe18e08b2b79
huanghao <zlzr200599@gmail.com>
2020-02-08 04:29:03 +0000

    comleted first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,12 @@
+public class HelloNumbers{
+	public static void main(String[] args){
+		int i = 0;
+		int sum = 0;
+		while(i < 10){
+			sum = sum + i;
+			System.out.print(sum + " ");
+			i = i + 1;
+		}
+		System.out.print("\n");
+	}
+}
